68a3b1d893b8bf6a7a3464c9629494c60cf88fb2
Mufei Li <mufeili1996@gmail.com>
2019-09-01 16:42:52 +0800

    Update

---
+++ dataset.py
@@ -0,0 +1,207 @@
+import os
+import numpy as np
+import pickle
+import torch
+from dgl.data.chem.utils import smile_to_bigraph
+from itertools import accumulate
+
+from .utils import mkdir_p
+
+class PCBADataset(object):
+    """
+    Parameters
+    ----------
+    file : str
+        Path to data file. The first line of the file contains 'smiles task1 task2...'.
+        Each rest line contains one datapoint in the form of 'smiles label_for_task1
+        label_for_task2...'.
+    chunk_size : int
+        For working with graph neural networks with DGL, we need to construct DGLGraph
+        for each molecule and featurize their atoms and/or bonds. Since the construction
+        itself is not cheap, we want to perform graph construction only once and save the
+        constructed graphs for later use. Different from fingerprint features, we cannot
+        save them in a txt file but need to serialize them with pickle. If the dataset is
+        too large, we cannot save them into a single file as they might be too large to
+        load into memory at once. For a workaround, we save the graphs into multiple files,
+        where each one can contain up to chunk_size graphs.
+    """
+    def __init__(self, file='data/pcba.txt', chunk_size=10000):
+        dir_processed = 'data/processed'
+        self.chunk_size = chunk_size
+        if os.path.isdir(dir_processed):
+            preprocessed = True
+        else:
+            preprocessed = False
+            mkdir_p(dir_processed)
+            self.chunk_id = 0
+            self.graphs = []
+
+        with open(file, 'r') as f:
+            headline = f.readline().strip()
+            self.tasks = headline.split('\t')[1:]
+            self.num_mols = 0
+
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                self.num_mols += 1
+                if not preprocessed:
+                    print('Processing molecule {:d}\n'.format(self.num_mols))
+                    self._process_molecule(line)
+
+        if not preprocessed:
+            self._dump_chunk()
+
+    def _process_molecule(self, line):
+        """
+        Parameters
+        ----------
+        line : str
+            SMILES and task labels for a molecule
+        """
+        elements = line.strip().split('\t')
+        smile = elements[0]
+        labels = np.array(elements[1:]).astype(np.float32)
+        is_nan = np.isnan(labels)
+        labels[is_nan] = 0
+        labels = torch.from_numpy(labels)
+        mask = torch.from_numpy(
+            (~is_nan).astype(np.float32))
+
+        g = smile_to_bigraph(smile)
+        g.smile = smile
+        g.mask = mask
+        g.labels = labels
+        self.graphs.append(g)
+        if len(self.graphs) == self.chunk_size:
+            self._dump_chunk()
+
+    def _dump_chunk(self):
+        """Save a chunk of molecules."""
+        if len(self.graphs) == 0:
+            return
+
+        with open('data/processed/chunk_{:d}.pkl'.format(self.chunk_id), 'wb') as f:
+            pickle.dump(self.graphs, f)
+        self.graphs = []
+        self.chunk_id += 1
+
+    def __len__(self):
+        """
+        Returns
+        -------
+        int
+            Number of molecules in the dataset
+        """
+        return self.num_mols
+
+    def __getitem__(self, item):
+        """Get the datapoint with idx item
+
+        Returns
+        -------
+        str
+            Molecule in SMILES
+        DGLGraph
+            DGLGraph for the molecule
+        float32 torch tensor of shape (1, num_tasks)
+            Labels for the molecule
+        float32 torch tensor of shape (1, num_tasks)
+            Binary mask for indicating the existence of labels
+        """
+        chunk_id = item // self.chunk_size
+        with open('data/processed/chunk_{:d}.pkl'.format(chunk_id), 'rb') as f:
+            graphs = pickle.load(f)
+            id_in_chunk = item % self.chunk_size
+            g = graphs[id_in_chunk]
+        return g.smile, g, g.labels, g.mask
+
+class Subset(object):
+    """Subset of a dataset at specified indices
+    Code adapted from PyTorch.
+
+    Parameters
+    ----------
+    dataset
+        dataset[i] should return the ith datapoint
+    indices : list
+        List of datapoint indices to construct the subset
+    """
+    def __init__(self, dataset, indices, num_tasks=128):
+        self.dataset = dataset
+        self.indices = indices
+        self.num_tasks = num_tasks
+        self._task_pos_weights = None
+
+    def __getitem__(self, item):
+        """Get the datapoint indexed by item
+
+        Returns
+        -------
+        tuple
+            datapoint
+        """
+        return self.dataset[self.indices[item]]
+
+    def __len__(self):
+        """Get subset size
+
+        Returns
+        -------
+        int
+            Number of datapoints in the subset
+        """
+        return len(self.indices)
+
+    @property
+    def task_pos_weights(self):
+        """We perform a weight balancing to address the imbalance
+        of positive and negative samples.
+
+        Returns
+        -------
+        self._task_pos_weights : torch float32 tensor of shape (self.num_tasks,)
+            self._task_pos_weights[i] gives the weight on positive samples of task
+            i in loss computation
+        """
+        if self._task_pos_weights is None:
+            task_total_count = torch.zeros(self.num_tasks)
+            task_pos_count = torch.zeros(self.num_tasks)
+
+            for i in self.indices:
+                s_i, g_i, label_i, mask_i = self.dataset[i]
+                task_total_count += mask_i
+                task_pos_count += label_i
+
+            self._task_pos_weights = (task_total_count - task_pos_count) / task_pos_count
+
+        return self._task_pos_weights
+
+def split_dataset(dataset, frac_list=None):
+    """
+    Parameters
+    ----------
+    dataset
+    frac_list : list of three floats
+        The proportion of data to use for training, validation and test.
+        If None, we will use [0.8, 0.1, 0.1].
+
+    Returns
+    -------
+    list
+        Consists of three subsets for training, validation and test
+    """
+    if frac_list is None:
+        frac_list = [0.8, 0.1, 0.1]
+
+    frac_list = np.array(frac_list)
+    assert np.allclose(np.sum(frac_list), 1.), \
+        'Expect frac_list sum to 1, got {:.4f}'.format(
+            np.sum(frac_list))
+    num_data = len(dataset)
+    lengths = (num_data * frac_list).astype(int)
+    lengths[-1] = num_data - np.sum(lengths[:-1])
+    indices = np.arange(num_data)
+    return [Subset(dataset, indices[offset - length:offset])
+            for offset, length in zip(accumulate(lengths), lengths)]
---
+++ main.py
@@ -0,0 +1,111 @@
+import datetime
+import time
+import torch
+
+from dgl import model_zoo
+from torch.nn import BCEWithLogitsLoss
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+from dataset import PCBADataset, split_dataset
+from utils import Meter, EarlyStopping, collate_molgraphs, set_random_seed
+
+def run_a_train_epoch(args, epoch, model, data_loader, loss_criterion, optimizer):
+    model.train()
+    train_meter = Meter()
+    for batch_id, batch_data in enumerate(data_loader):
+        smiles, bg, labels, mask = batch_data
+        atom_feats = bg.ndata.pop(args['atom_data_field'])
+        atom_feats, labels, mask = atom_feats.to(args['device']), \
+                                   labels.to(args['device']), \
+                                   mask.to(args['device'])
+        logits = model(bg, atom_feats)
+        # Mask non-existing labels
+        loss = (loss_criterion(logits, labels) * (mask != 0).float()).mean()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print('epoch {:d}/{:d}, batch {:d}/{:d}, loss {:.4f}'.format(
+            epoch + 1, args['num_epochs'], batch_id + 1, len(data_loader), loss.item()))
+        train_meter.update(logits, labels, mask)
+    train_roc_auc = train_meter.roc_auc_averaged_over_tasks()
+    print('epoch {:d}/{:d}, training roc-auc score {:.4f}'.format(
+        epoch + 1, args['num_epochs'], train_roc_auc))
+
+def run_an_eval_epoch(args, model, data_loader):
+    model.eval()
+    eval_meter = Meter()
+    with torch.no_grad():
+        for batch_id, batch_data in enumerate(data_loader):
+            smiles, bg, labels, mask = batch_data
+            atom_feats = bg.ndata.pop(args['atom_data_field'])
+            atom_feats, labels = atom_feats.to(args['device']), labels.to(args['device'])
+            logits = model(bg, atom_feats)
+            eval_meter.update(logits, labels, mask)
+    return eval_meter.roc_auc_averaged_over_tasks()
+
+def main(args):
+    args['device'] = "cuda" if torch.cuda.is_available() else "cpu"
+    set_random_seed()
+
+    dataset = PCBADataset(chunk_size=args['chunk_size'])
+    trainset, valset, testset = split_dataset(dataset)
+    train_loader = DataLoader(trainset, batch_size=args['batch_size'], collate_fn=collate_molgraphs)
+    val_loader = DataLoader(valset, batch_size=args['batch_size'], collate_fn=collate_molgraphs)
+    test_loader = DataLoader(testset, batch_size=args['batch_size'], collate_fn=collate_molgraphs)
+
+    if args['model'] == 'GCN':
+        model = model_zoo.chem.GCNClassifier(in_feats=args['in_feats'],
+                                             gcn_hidden_feats=args['gcn_hidden_feats'],
+                                             classifier_hidden_feats=args['classifier_hidden_feats'],
+                                             n_tasks=dataset.n_tasks)
+    elif args['model'] == 'GAT':
+        model = model_zoo.chem.GATClassifier(in_feats=args['in_feats'],
+                                             gat_hidden_feats=args['gat_hidden_feats'],
+                                             num_heads=args['num_heads'],
+                                             classifier_hidden_feats=args['classifier_hidden_feats'],
+                                             n_tasks=dataset.n_tasks)
+
+    loss_criterion = BCEWithLogitsLoss(pos_weight=torch.tensor(
+        dataset.task_pos_weights).to(args['device']), reduction='none')
+    optimizer = Adam(model.parameters(), lr=args['lr'])
+    stopper = EarlyStopping(patience=args['patience'])
+    model.to(args['device'])
+
+    t0 = time.time()
+
+    for epoch in range(args['num_epochs']):
+        # Train
+        run_a_train_epoch(args, epoch, model, train_loader, loss_criterion, optimizer)
+
+        # Validation and early stop
+        val_roc_auc = run_an_eval_epoch(args, model, val_loader)
+        early_stop = stopper.step(val_roc_auc, model)
+        print('epoch {:d}/{:d}, validation roc-auc score {:.4f}, best validation roc-auc score {:.4f}'.format(
+            epoch + 1, args['num_epochs'], val_roc_auc, stopper.best_score))
+        if early_stop:
+            break
+
+    t1 = time.time()
+    print('It took {} to finish training for an epoch.'.format(datetime.timedelta(seconds=t1 - t0)))
+
+    if not args['pre_trained']:
+        stopper.load_checkpoint(model)
+    test_roc_auc = run_an_eval_epoch(args, model, test_loader)
+    print('test roc-auc score {:.4f}'.format(test_roc_auc))
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Molecule Classification')
+    parser.add_argument('-m', '--model', type=str, choices=['GCN', 'GAT'],
+                        help='Model to use')
+    parser.add_argument('-c', '--chunk-size', type=int, default=10000,
+                        help='Number of preprocessed molecules in each pickle file')
+    parser.add_argument('-b', '--batch-size', type=int, default=128,
+                        help='Batch size for data loading')
+    parser.add_argument('-n', '--num-epochs', type=int, default=1,
+                        help='Max number of epochs to train the model')
+    args = parser.parse_args().__dict__
+
+    main(args)
